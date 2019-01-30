cc9f2b44563e7d51d5c215dc7ca4edaecf6dcd95
Edoardo <edoardo.giacomelli@barsanti.edu.it>
2019-01-30 13:35:24 +0100

    semaforo finito

--- sketch_semaforo/sketch_semaforo.ino
+++ sketch_semaforo/sketch_semaforo.ino
@@ -1,38 +1,45 @@
+int VERDE1 = 12;
+int GIALLO1 = 10;
+int ROSSO1 = 8;
+int VERDE2 = 4;
+int GIALLO2 = 2;
+int ROSSO2 = 0;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode (12, OUTPUT);    //VERDE
-  pinMode (10, OUTPUT);    //GIALLO
-  pinMode (8, OUTPUT);     //ROSSO
-  pinMode (4, OUTPUT);     //VERDE
-  pinMode (2, OUTPUT);     //GIALLO
-  pinMode (0, OUTPUT);     //ROSSO
+  pinMode (VERDE1, OUTPUT);    //VERDE
+  pinMode (GIALLO1, OUTPUT);    //GIALLO
+  pinMode (ROSSO1, OUTPUT);     //ROSSO
+  pinMode (VERDE2, OUTPUT);     //VERDE
+  pinMode (GIALLO2, OUTPUT);     //GIALLO
+  pinMode (ROSSO2, OUTPUT);     //ROSSO
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite (12, HIGH);  //accendo il verde
-  digitalWrite (0, HIGH);   //accendo il rosso
+  digitalWrite (VERDE1, HIGH);  //accendo il verde
+  digitalWrite (ROSSO2, HIGH);   //accendo il rosso
   delay (5000);
-  lampeggia(12, 500, 4);    //faccio lampeggiare il verde 4 volte per 0,5 sec
-  digitalWrite (12, LOW);   //spengo il verde
-  digitalWrite (10, HIGH);  //accendo il giallo
-  digitalWrite (2, HIGH);   //accendo il giallo
+  lampeggia(VERDE1, 500, 4);    //faccio lampeggiare il verde 4 volte per 0,5 sec
+  digitalWrite (VERDE1, LOW);   //spengo il verde
+  digitalWrite (GIALLO1, HIGH);  //accendo il giallo
+  digitalWrite (GIALLO2, HIGH);   //accendo il giallo
   delay (2000);
-  digitalWrite (10, LOW);   //spengo il giallo
-  digitalWrite (0, LOW);    //spengo il rosso
-  digitalWrite (2, LOW);    //spengo il giallo
+  digitalWrite (GIALLO1, LOW);   //spengo il giallo
+  digitalWrite (ROSSO2, LOW);    //spengo il rosso
+  digitalWrite (GIALLO2, LOW);    //spengo il giallo
   delay (0);
-  digitalWrite (8, HIGH);   //accendo il rosso
-  digitalWrite (4, HIGH);   //accendo il verde
+  digitalWrite (ROSSO1, HIGH);   //accendo il rosso
+  digitalWrite (VERDE2, HIGH);   //accendo il verde
   delay (5000);
-  lampeggia(4, 500, 4);     //faccio lampeggiare il verde 4 volte per 0,5 sec
-  digitalWrite (4, LOW);    //spengo il verde
-  digitalWrite (2, HIGH);   //accendo il giallo
-  digitalWrite (10, HIGH);  //accendo il giallo
+  lampeggia(VERDE2, 500, 4);     //faccio lampeggiare il verde 4 volte per 0,5 sec
+  digitalWrite (VERDE2, LOW);    //spengo il verde
+  digitalWrite (GIALLO2, HIGH);   //accendo il giallo
+  digitalWrite (GIALLO1, HIGH);  //accendo il giallo
   delay (2000);
-  digitalWrite (2, LOW);    //spengo il giallo
-  digitalWrite (10, LOW);   //spengo il giallo
-  digitalWrite (8, LOW);    //spengo il rosso
+  digitalWrite (GIALLO2, LOW);    //spengo il giallo
+  digitalWrite (GIALLO1, LOW);   //spengo il giallo
+  digitalWrite (ROSSO1, LOW);    //spengo il rosso
 }
 
 void lampeggia(int led, int rit, int a) {  
