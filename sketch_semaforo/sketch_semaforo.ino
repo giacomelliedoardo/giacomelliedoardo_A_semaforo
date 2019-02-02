6fc07b01dc9e3aa81bc23cc8c347f216f81098e9
Edoardo <edoardo.giacomelli@barsanti.edu.it>
2019-02-02 09:48:12 +0100

    semaforo input da finire

--- sketch_semaforo/sketch_semaforo.ino
+++ sketch_semaforo/sketch_semaforo.ino
@@ -4,15 +4,23 @@ int ROSSO1 = 8;
 int VERDE2 = 4;
 int GIALLO2 = 2;
 int ROSSO2 = 0;
+//String numLampeggi = "";
+//int richiesta = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode (VERDE1, OUTPUT);    //VERDE
   pinMode (GIALLO1, OUTPUT);    //GIALLO
   pinMode (ROSSO1, OUTPUT);     //ROSSO
+  
   pinMode (VERDE2, OUTPUT);     //VERDE
   pinMode (GIALLO2, OUTPUT);     //GIALLO
   pinMode (ROSSO2, OUTPUT);     //ROSSO
+  
+  Serial.print("Quanti lampeggi verdi?");
+  while(Serial.available == 0){};
+  numLampeggi = Serial.readString().toInt();
+  
 }
 
 void loop() {
@@ -20,7 +28,12 @@ void loop() {
   digitalWrite (VERDE1, HIGH);  //accendo il verde
   digitalWrite (ROSSO2, HIGH);   //accendo il rosso
   delay (5000);
-  lampeggia(VERDE1, 500, 4);    //faccio lampeggiare il verde 4 volte per 0,5 sec
+  lampeggia (VERDE1, 500, 4);
+  /*if(richiesta == 0)
+  {
+    richiestaLampeggi();
+    lampeggia();
+  }*/
   digitalWrite (VERDE1, LOW);   //spengo il verde
   digitalWrite (GIALLO1, HIGH);  //accendo il giallo
   digitalWrite (GIALLO2, HIGH);   //accendo il giallo
@@ -32,7 +45,12 @@ void loop() {
   digitalWrite (ROSSO1, HIGH);   //accendo il rosso
   digitalWrite (VERDE2, HIGH);   //accendo il verde
   delay (5000);
-  lampeggia(VERDE2, 500, 4);     //faccio lampeggiare il verde 4 volte per 0,5 sec
+  lampeggia (VERDE2, 500, 4);
+  /*if(richiesta == 0)
+  {
+    richiestaLampeggi();
+    lampeggia();
+  }*/
   digitalWrite (VERDE2, LOW);    //spengo il verde
   digitalWrite (GIALLO2, HIGH);   //accendo il giallo
   digitalWrite (GIALLO1, HIGH);  //accendo il giallo
@@ -49,4 +67,11 @@ void lampeggia(int led, int rit, int a) {
     delay (rit);
     digitalWrite (led, HIGH);
   }
+
+/*void richiestaLampeggi(){
+  Serial.print("Quanti lampeggi verdi?");
+  while(Serial.available == 0){};
+  numLampeggi = Serial.readString().toInt();
+}*/
+
 }
