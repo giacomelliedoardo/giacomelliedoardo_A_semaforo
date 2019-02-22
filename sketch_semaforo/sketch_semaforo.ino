0ffffe2754929c318ed3d5eee042332f8ec74c78
Edoardo <edoardo.giacomelli@barsanti.edu.it>
2019-02-22 15:49:25 +0100

    piccoli aggiustamenti

--- sketch_semaforo/sketch_semaforo.ino
+++ sketch_semaforo/sketch_semaforo.ino
@@ -18,9 +18,9 @@ void setup() {
   pinMode (VERDE2, OUTPUT);     //VERDE
   pinMode (GIALLO2, OUTPUT);    //GIALLO
   pinMode (ROSSO2, OUTPUT);     //ROSSO
-  
+  //domande in input
   Serial.begin(9600);
-  String domandaLamp      = "Quanti lampeggi deve fare il verde? (millisecondi)";
+  String domandaLamp      = "Quanti lampeggi deve fare il verde?";
   String domandaTempoLamp = "Quanto durano i lampeggi? (millisecondi)";
   String domandaVerde     = "Quanto resta acceso il verde? (millisecondi)";
   String domandaGiallo    = "Quanto resta acceso il giallo? (millisecondi)";
@@ -29,32 +29,31 @@ void setup() {
   verdeHigh               = input(verdeHigh, domandaVerde);
   gialloHigh              = input(gialloHigh, domandaGiallo);
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite (VERDE1, HIGH); 
-  digitalWrite (ROSSO2, HIGH);   
+  digitalWrite (VERDE1, HIGH);
+  digitalWrite (ROSSO2, HIGH);
   delay (verdeHigh);
   lampeggia(VERDE1);
   digitalWrite (VERDE1, LOW);
-  digitalWrite (GIALLO1, HIGH);  
-  digitalWrite (GIALLO2, HIGH);   
+  digitalWrite (GIALLO1, HIGH);
+  digitalWrite (GIALLO2, HIGH);
   delay (gialloHigh);
-  digitalWrite (GIALLO1, LOW);      
-  digitalWrite (GIALLO2, LOW);   
+  digitalWrite (GIALLO1, LOW);
+  digitalWrite (GIALLO2, LOW);
   digitalWrite (ROSSO2, LOW);
-  digitalWrite (ROSSO1, HIGH);   
-  digitalWrite (VERDE2, HIGH);  
-  delay (verdeHigh);  
+  digitalWrite (ROSSO1, HIGH);
+  digitalWrite (VERDE2, HIGH);
+  delay (verdeHigh);
   lampeggia(VERDE2);
   digitalWrite (VERDE2, LOW);
-  digitalWrite (GIALLO2, HIGH); 
+  digitalWrite (GIALLO2, HIGH);
   digitalWrite (GIALLO1, HIGH);
-  digitalWrite (ROSSO1, HIGH); 
+  digitalWrite (ROSSO1, HIGH);
   delay (gialloHigh);
-  digitalWrite (GIALLO2, LOW);  
-  digitalWrite (GIALLO1, LOW);   
-  digitalWrite (ROSSO1, LOW); 
+  digitalWrite (GIALLO2, LOW);
+  digitalWrite (GIALLO1, LOW);
+  digitalWrite (ROSSO1, LOW);
 }
 //lampeggio led 
 void lampeggia(int led) {  
@@ -62,8 +61,7 @@ void lampeggia(int led) {
     delay (tempoLampeggi);
     digitalWrite (led, LOW);
     delay (tempoLampeggi);
-    digitalWrite (led, HIGH);
-  }
+    digitalWrite (led, HIGH);}
 }
 //domande input
 int input(int condizione, String domanda) {
